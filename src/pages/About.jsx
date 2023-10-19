103ede3b4bce96d9e20b2e3f3da77c697010703c
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-10-18 22:19:45 -0500

    Link Component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,13 @@
+import { Link } from 'react-router-dom';
+
 const About = () => {
-  return <h1>About</h1>;
+  return (
+    <div>
+      <h1>About</h1>
+      <br />
+      <Link to="/">Back to Home</Link>
+    </div>
+  );
 };
 
 export default About;
--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,5 +1,26 @@
+import { Link } from 'react-router-dom';
+
 const HomeLayout = () => {
-  return <h1>Home Layout</h1>;
+  return (
+    <div>
+      <h1>Home Layout</h1>
+      <br />
+
+      {/*Link component - Designed to navigate around the project only */}
+      <Link to="/about">About Page</Link>
+      <br />
+
+      {/* For external sites, HTML link element should be used */}
+      <a
+        href="https://github.com/praveen-1995"
+        target="_blank"
+        rel="noreferrer"
+        style={{ marginTop: '1rem', display: 'inline-block' }}
+      >
+        Github
+      </a>
+    </div>
+  );
 };
 
 export default HomeLayout;
