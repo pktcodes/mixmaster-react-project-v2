03b772aecc64906fb5ee9074cf70d7cb1804025a
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-10-19 18:14:49 -0500

    Nested Pages

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+  return (
+    <h1
+      style={{
+        marginBottom: '1rem',
+        padding: '1rem 0',
+        background: 'springgreen',
+      }}
+    >
+      Navbar
+    </h1>
+  );
+};
+
+export default Navbar;
--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,24 +1,11 @@
-import { Link } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
+import Navbar from '../components/Navbar';
 
 const HomeLayout = () => {
   return (
     <div>
-      <h1>Home Layout</h1>
-      <br />
-
-      {/*Link component - Designed to navigate around the project only */}
-      <Link to="/about">About Page</Link>
-      <br />
-
-      {/* For external sites, HTML link element should be used */}
-      <a
-        href="https://github.com/praveen-1995"
-        target="_blank"
-        rel="noreferrer"
-        style={{ marginTop: '1rem', display: 'inline-block' }}
-      >
-        Github
-      </a>
+      <Navbar />
+      <Outlet />
     </div>
   );
 };
