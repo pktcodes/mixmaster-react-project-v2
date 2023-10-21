3937efa791127a47a0dcf93f92237258be726f36
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-10-20 23:47:39 -0500

    Styled Components - Setup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,19 @@
 import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledBtn = styled.button`
+  margin-bottom: 1rem;
+  background: red;
+  color: white;
+  font-size: 2rem;
+  padding: 1rem;
+  text-transform: capitalize;
+`;
 
 const Navbar = () => {
   return (
     <nav>
+      <StyledBtn>styled btn</StyledBtn>
       <div className="nav-center">
         <span className="logo">MixMaster</span>
         <div className="nav-links">
