e91d546e78aead6ea56012349414fa7e3fdc5a6b
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-10-25 23:52:51 -0500

    Error Page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,5 +1,31 @@
+import { Link, useRouteError } from 'react-router-dom';
+import img from '../assets/not-found.svg';
+import Wrapper from '../assets/wrappers/ErrorPage';
+
 const Error = () => {
-  return <h1>Error</h1>;
+  const errorResponse = useRouteError();
+  console.log(errorResponse);
+
+  if (errorResponse.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={img} alt={errorResponse.statusText} />
+          <h3>ohh!</h3>
+          <h3>{"we can't seem to find the page you are looking for"}</h3>
+          <Link to="/">back to home</Link>
+        </div>
+      </Wrapper>
+    );
+  }
+
+  return (
+    <Wrapper>
+      <div>
+        <h3>something went wrong</h3>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Error;
--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -3,12 +3,12 @@ import Navbar from '../components/Navbar';
 
 const HomeLayout = () => {
   return (
-    <div>
+    <>
       <Navbar />
       <section className="page">
         <Outlet />
       </section>
-    </div>
+    </>
   );
 };
 
