3fce085fddab59eb5ecab9838869b375ce77d63f
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-11-06 23:33:00 -0600

    Single Page Error

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,3 +5,4 @@ export { default as Error } from './Error';
 export { default as HomeLayout } from './HomeLayout';
 export { default as Landing } from './Landing';
 export { default as Newsletter } from './Newsletter';
+export { default as SinglePageError } from './SinglePageError';
