7872480ff70516267a347de7f31ad0c6c396afbb
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-11-15 00:05:31 -0600

    React Toastify - Setup

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import './index.css';
+
+toast('Hello Toast');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <ToastContainer position="top-center" autoClose="2000" />
   </React.StrictMode>
 );
